d7050ee0bd2fe2e2d237b1b04e36992e29b04683
Nigilen <nigilen@gmail.com>
2021-08-12 23:14:08 +0200

    bla

--- js/backend.js
+++ js/backend.js
@@ -1,13 +1,51 @@
 let xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
 let arr;
 
+
+let onError = function (message) {
+  console.log(message);
+}
+
+let onSuccess = function (abb) {
+  console.log(abb);
+};
+
 xhr.addEventListener('load', function(evt) {
-  console.log(xhr.readyState);
-  console.log(xhr.status + ' ' + xhr.statusText);
+  let error;
+  switch (xhr.status) {
+    case 200:
+      onSuccess(xhr.response);
+      break;
+    case 400:
+      error: 'Неверный запрос';
+      break;
+    case 401:
+      error: 'Пользователь не авторизирован';
+      break;
+    case 404:
+      error: 'Ничего не найдено';
+      break;
+    
+      default: 
+        error: 'Статус ответа: : ' + xhr.status + ' ' + xhr.statusText;
+    };
+
+    if (error) {
+      onError(error);
+    };
+
+
+  // console.log(xhr.readyState);
+  // console.log(xhr.status + ' ' + xhr.statusText);
   // console.log(xhr);
   // console.log(xhr.responseText);
-  arr = JSON.parse(xhr.responseText);
-  console.log(arr[1].colorCoat);
+  // console.log(xhr.response);
+  // try {
+  //   console.log(JSON.parse(xhr.responseText));
+  // } catch (err) {
+  //   console.err(err.message);
+  // }
 });
 
 
